809b65ab8dab255afbe35952f0d8d3e663c38064
jcaole <jeremy_1072@yahoo.com>
2023-05-10 09:57:00 -0500

    saving progress

--- project-resume/src/components/contact/ContactLeft.js
+++ project-resume/src/components/contact/ContactLeft.js
@@ -13,7 +13,7 @@ const ContactLeft = () => {
       <div className="flex flex-col gap-4">
         <h3 className="text-3xl font-bold text-white">Jeremy Caole</h3>
         <p className="text-lg font-normal text-gray-400">
-          Aspiring Developer
+          UWF Graduate
         </p>
         <p className="text-base text-gray-400 tracking-wide">
         </p>
